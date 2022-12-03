94f14c700d7216a8e4fbda3e0e7b360cd79e23bb
a1yak <alyak.grt@gmail.com>
2022-12-03 11:09:33 +0200

    tests

--- src/test/java/com/example/crawler/controller/CrawlerControllerTest.java
+++ src/test/java/com/example/crawler/controller/CrawlerControllerTest.java
@@ -1,25 +1,61 @@
 package com.example.crawler.controller;
 
+import com.example.crawler.model.Site;
+import com.example.crawler.service.CrawlerService;
+import jakarta.servlet.ServletContext;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import java.util.List;
+import org.springframework.web.context.WebApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@ExtendWith(MockitoExtension.class)
 @WebMvcTest(CrawlerController.class)
 class CrawlerControllerTest {
 
     @Autowired
-    private MockMvc mvc;
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
 
     @Test
-    void startCrawler(){
+    public void givenWac_whenServletContext_thenItProvidesGreetController() {
+        ServletContext servletContext = webApplicationContext.getServletContext();
+
+        Assertions.assertNotNull(servletContext);
+        Assertions.assertTrue(servletContext instanceof MockServletContext);
+        Assertions.assertNotNull(webApplicationContext.getBean("crawlerController"));
+    }
+    @Test
+    void startCrawlerShouldReturnOk() throws Exception {
+
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/crawler?url=https://liquipedia.net/dota2/Alliance")).andExpect(status().isOk());
 
     }
 }
